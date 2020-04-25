b2296866d06bb9804204952eaf07baad05aa7cbd
kshitij.burman <kshitij.burman@tech9.com>
2020-04-25 22:32:57 +0530

    Few changes.

--- app/workers/process_reddit_response_worker.rb
+++ app/workers/process_reddit_response_worker.rb
@@ -8,6 +8,13 @@ class ProcessRedditResponseWorker
     concurrency: { limit: 1 },
   })
 
+  KIND_MAPPING = {
+    't1' => CreateRedditCommentService,
+    't2' => CreateRedditAccountService,
+    't3' => CreateRedditLinkService,
+    't5' => CreateRedditSubredditService,
+  }.freeze
+
   def perform(redis_key)
     data = REDIS_POOL.with { |c| c.get(redis_key) }
     data = JSON.parse(data)
@@ -17,6 +24,8 @@ class ProcessRedditResponseWorker
   private
 
   def process(data)
+    return if data.nil?
+
     if data.is_a?(Hash) && data.key?('kind')
       handle_reddit_obj(data)
     elsif data.is_a?(Array)
@@ -27,6 +36,14 @@ class ProcessRedditResponseWorker
   end
 
   def handle_reddit_obj(data)
-    puts(data)
+    if KIND_MAPPING.key?(data['kind'])
+      KIND_MAPPING[data['kind']].call(data['data'])
+    elsif data['kind'] == 'Listing'
+      process(data.dig('data', 'children'))
+    elsif data['kind'] == 'more'
+      nil
+    else
+      raise data['kind']
+    end
   end
 end
--- app/workers/reddit_api_fetch_worker.rb
+++ app/workers/reddit_api_fetch_worker.rb
@@ -12,10 +12,11 @@ class RedditApiFetchWorker
 
   def perform(url, redis_key_ns, redis_key, fetcher_opt)
     api_response = RedditApiFetchService.call(url, fetcher_opt)
+    api_response_key = "#{redis_key_ns}:#{redis_key}"
     REDIS_POOL.with do |conn|
-      conn.set("#{redis_key_ns}:#{redis_key}", api_response[:data].to_json)
+      conn.set(api_response_key, api_response[:data].to_json)
     end
-
+    ProcessRedditResponseWorker.perform_async(api_response_key)
 
     api_response[:next_links].each do |link_key, link|
       batch.jobs do
